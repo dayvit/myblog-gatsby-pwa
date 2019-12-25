aee5769f5738387ac4a31060ca15fafd796bb32c
Dayvit dos Reis Siqueira <dayvitgit@gmail.com>
2019-12-25 01:54:16 -0300

    Social Links Components

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "styled-icons/fa-brands/Github"
+import { Linkedin } from "styled-icons/boxicons-logos/Linkedin"
+import { Instagram } from "styled-icons/boxicons-logos/Instagram"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+
+const Icons = {
+  Github,
+  Linkedin,
+  Instagram,
+  Twitter,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/dayvit",
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/dayvit/",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/dayvitst/",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/dayvitst",
+  }
+]
+
+export default links
