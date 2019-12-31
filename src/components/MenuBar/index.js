9ea9d76dd9b65f2806a101bfc71cc460eae09383
Dayvit dos Reis Siqueira <dayvitgit@gmail.com>
2019-12-31 06:22:32 -0300

    Keeping elements fixed while animating

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -5,6 +5,7 @@ import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
 import { LightBulb as Light } from "styled-icons/octicons/LightBulb"
 import { Grid } from "styled-icons/boxicons-solid/Grid"
 import { ThList as List } from "styled-icons/typicons/ThList"
+import getThemeColor from "../../utils/getThemeColor"
 
 import * as S from "./styled"
 
@@ -30,7 +31,7 @@ const MenuBar = () => {
           to="/"
           cover
           direction="right"
-          bg="#16202c"
+          bg={getThemeColor()}
           duration={0.6}
           title="Voltar para Home"
         >
@@ -42,7 +43,7 @@ const MenuBar = () => {
           to="/search/"
           cover
           direction="right"
-          bg="#16202c"
+          bg={getThemeColor()}
           duration={0.6}
           title="Pesquisar"
         >
